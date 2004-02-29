697d64406b478bd3bbd219397ffcb54430935220
jonasbn <jonasbn@logiclab.dk>
2004-02-29 10:48:46 +0000

    Initial revision
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/MTS/trunk@5668 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ lib/Module/Template/Setup.pm
@@ -0,0 +1,25 @@
+package Module::Template::Setup;
+
+# $Id: Setup.pm,v 1.1 2004-02-29 10:48:46 jonasbn Exp $
+
+1;
+
+__END__
+
+=head1 NAME
+
+Module::Template::Setup - 
+
+=head1 SYNOPSIS
+
+=head1 ABSTRACT
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT
+
+=cut
